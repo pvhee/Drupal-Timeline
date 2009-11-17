02b95b368962114fbf16e0690edbb1a45d775475
xamanu <xamanu@consciente.de>
2009-11-17 23:33:17 +0100

    General plugin logic. Half way. No plugins and no instance calls yet.

---
+++ plugins/TimelineNodePlugin.php
@@ -0,0 +1,15 @@
+<?php
+// $Id: 
+
+/**
+ * @file
+ * Class TimelineNodePlugin
+ */
+
+/**
+ * This class provides a plugin to handle nodes timestamp fields (created and updated)
+ * to use with the timeline module
+ */
+class TimelineNodePlugin extends TimelinePlugin {
+  
+}
